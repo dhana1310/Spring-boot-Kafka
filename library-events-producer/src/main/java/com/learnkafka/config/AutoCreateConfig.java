4f8dd0fcb85d1dd7772588decb2625fb5fd9bf8c
Dhananjay Sharma <sharmadhananjay42@gmail.com>
2020-04-27 21:34:19 +0530

    Spring boot kafka code added

---
+++ library-events-producer/src/main/java/com/learnkafka/config/AutoCreateConfig.java
@@ -0,0 +1,27 @@
+package com.learnkafka.config;
+
+import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+@Profile("local")
+public class AutoCreateConfig {
+
+    @Bean
+    public NewTopic libraryEvents(){
+        return TopicBuilder.name("library-events1")
+                .partitions(3)
+                .replicas(3)
+                //.config("min.insync.replicas", "2")
+                .build();
+    }
+
+}
